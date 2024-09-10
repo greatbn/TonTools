19379274d8028dc42f25c7a39f5065b1c33f32d6
sapd <sapd@vccloud.vn>
2024-09-10 21:34:29 +0700

    upgrade graphql-query

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 import setuptools
 
-requirements = ["tonsdk>=1.0.13", "ton>=0.26", "aiohttp>=3.8.1", "setuptools>=65.3.0", "requests>=2.28.1", "pytonlib>=0.0.46", "graphql-query==1.0.3"]
+requirements = ["tonsdk>=1.0.13", "ton>=0.26", "aiohttp>=3.8.1", "setuptools>=65.3.0",
+                "requests>=2.28.1", "pytonlib>=0.0.46", "graphql-query==1.4.0"]
 
 setup(
     name='TonTools',
